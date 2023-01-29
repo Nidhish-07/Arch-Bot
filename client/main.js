b4a99eb6cc490d1fb0d6f5e00c0c3df27fe8aeca
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-29 15:55:51 +0530

    frontend logic

--- client/main.js
+++ client/main.js
@@ -1,5 +1,87 @@
-import Bot from "/icons/botL.png"
-import User from "/icons/userL.png"
+import Bot from "/icons/botL.png";
+import User from "/icons/userL.png";
+import "./style.css";
 
-const form = document.querySelector('form');
-const chatText=document.querySelector("#chat-text");
+const form = document.querySelector("form");
+const chatText = document.querySelector("#chat-text");
+
+let timeInterval;
+
+const loader = (ele) => {
+  ele.textContent = "";
+
+  timeInterval = setInterval(() => {
+    ele.textContent += ".";
+
+    if (ele.textContent === ".....") {
+      ele.textContent = "";
+    }
+  }, 500);
+};
+
+const answeredText = (ele, text) => {
+  let i = 0;
+
+  let interval = setInterval(() => {
+    if (i < text.length) {
+      ele.innerHTML += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 25);
+};
+
+const generateID = () => {
+  const timestamp = new Date.now();
+  const randomNumber = Math.random();
+  const hexaString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexaString}`;
+};
+
+const chat = (isAI, val, uniqueID) => {
+  return `
+  <div class="${isAI && "ai"}">
+  <div class="chat">
+  <div class="profile">
+  <img src="${isAI ? Bot : User}" alt="${isAI ? "bot" : "user"}" />
+  </div>
+  <div class="message" id=${uniqueID}>${val}</div>
+  </div>
+  </div>
+  `;
+};
+
+
+const submitHandler = async (event) => {
+  event.preventDefault();
+
+  const data = new FormData(form)
+
+  chatText.innerHTML += chat(false, data.get('prompt'))
+  form.reset()
+
+  const uniqueID = generateID()
+  chatText.innerHTML += chat(true, "", uniqueID)
+
+  chatText.scrollTop = chatText.scrollHeight
+
+  const messageM = document.getElementById(uniqueID)
+
+  loader(messageM)
+
+  const response = await fetch("http://localhost:8080", {
+    method: 'POST', headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ prompt: data.get('prompt') })
+  })
+}
+
+form.addEventListener('submit', submitHandler)
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    submitHandler(e)
+  }
+})
