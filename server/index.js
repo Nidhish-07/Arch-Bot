f693908b335ab9d915490bcac111270b0bf3e389
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-03-15 14:58:38 +0530

    organization added

--- server/index.js
+++ server/index.js
@@ -6,44 +6,43 @@ import { Configuration, OpenAIApi } from "openai";
 
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-    organization: "org-MyiOfJnLrKdoCDW0K0ZcKegD",
+  apiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.OPENAI_ORGANIZATION,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-    res.status(200).send("hello");
+  res.status(200).send("hello");
 });
 
 app.post("/", async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
+  try {
+    const prompt = req.body.prompt;
 
-        const response = await openai.createCompletion({
-            prompt: `${prompt}`, model: "text-davinci-002",
-            temperature: 0.9,
-            max_tokens: 150,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0.6,
-        });
+    const response = await openai.createCompletion({
+      prompt: `${prompt}`,
+      model: "text-davinci-002",
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+    });
 
-        res.status(200).send({ bot: response.data.choices[0].text })
-    } catch (error) {
-
-        console.log(error);
-        res.status(500).send(error)
-    }
+    res.status(200).send({ bot: response.data.choices[0].text });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 app.listen(port, () => {
-    console.log("Server listening on port http://localhost:" + port);
-})
+  console.log("Server listening on port http://localhost:" + port);
+});
